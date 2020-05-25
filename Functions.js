63cd06e2b1aa5b9c46f82da9ffc8739a29486a99
Bappi Saha <bappi35-2097@diu.edu.bd>
2020-05-25 23:48:26 +0600

    factorial complete

---
+++ Functions.js
@@ -0,0 +1,33 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+/*
+ * Create the function factorial here
+ */
+function factorial(n){
+    let fact=1;
+    while(n--){
+        fact*=Number(n)+1;
+    }
+    return fact;
+}
