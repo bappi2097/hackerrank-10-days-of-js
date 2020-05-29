0d3d37059ae12fcdb92a45f3544987ab35f53986
Bappi Saha <bappi35-2097@diu.edu.bd>
2020-05-29 19:14:46 +0600

    Count Objects

---
+++ Count Objects.js
@@ -0,0 +1,38 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((string) => {
+      return string.trim();
+    });
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Return a count of the total number of objects 'o' satisfying o.x == o.y.
+ *
+ * Parameter(s):
+ * objects: an array of objects with integer properties 'x' and 'y'
+ */
+function getCount(objects) {
+  var count = 0;
+  for (let obj in objects) if (objects[obj].x == objects[obj].y) count++;
+  return count;
+}
