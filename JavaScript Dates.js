cddee7e333224694dc6d9cef54969dec0795b470
Bappi Saha <bappi35-2097@diu.edu.bd>
2020-05-30 14:56:17 +0600

    Javascript Dates complete

---
+++ JavaScript Dates.js
@@ -0,0 +1,52 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((string) => {
+      return string.trim();
+    });
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+// The days of the week are: "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+function getDayName(dateString) {
+  let dayName = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  // Write your code here
+  let date = new Date(dateString);
+  return dayName[date.getDay()];
+}
+
+function main() {
+  const d = +readLine();
+
+  for (let i = 0; i < d; i++) {
+    const date = readLine();
+
+    console.log(getDayName(date));
+  }
+}
